e20c09c75f7ae9e4491223a2062948e7af766895
lypandrew87 <lypandrew87@gmail.com>
2021-09-21 23:46:17 -0500

    Code cleanup

--- Emergent_Software_Version_Software_Api/Managers/SoftwareManager.cs
+++ Emergent_Software_Version_Software_Api/Managers/SoftwareManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using Emergent_Software_Version_Software_Api.Interfaces;
 using Emergent_Software_Version_Software_Api.Models;
 
@@ -20,37 +19,43 @@ namespace Emergent_Software_Version_Software_Api.Managers
             var filteredSoftware = new List<Software>();
             var allSoftware = softwareRepository.GetAllSoftware();
 
-            var inputVersions = Array.ConvertAll(version.Split('.'), int.Parse).ToList();
+            var inputVersions = Array.ConvertAll(version.Split('.'), int.Parse);
 
             foreach (var software in allSoftware)
             {
-                var currentSoftwareVersions = Array.ConvertAll(software.Version.Split('.'), int.Parse).ToList();
+                var softwareVersion = Array.ConvertAll(software.Version.Split('.'), int.Parse);
 
-
-                if (currentSoftwareVersions.Equals(inputVersions))
+                if (IsHigherVersion(softwareVersion, inputVersions))
                 {
-                    continue; 
-                }
-
-                if (inputVersions.ElementAtOrDefault(0) < currentSoftwareVersions.ElementAtOrDefault(0))
-                {
-                    filteredSoftware.Add(software);
-                }else if (inputVersions.ElementAtOrDefault(0) == currentSoftwareVersions.ElementAtOrDefault(0))
-                {
-                    if (inputVersions.ElementAtOrDefault(1) < currentSoftwareVersions.ElementAtOrDefault(1))
-                    {
-                        filteredSoftware.Add(software);
-                    }else if (inputVersions.ElementAtOrDefault(1) == currentSoftwareVersions.ElementAtOrDefault(1))
-                    {
-                        if (inputVersions.ElementAtOrDefault(2) < currentSoftwareVersions.ElementAtOrDefault(2))
-                        {
-                            filteredSoftware.Add(software);
-                        }
-                    }
+                    filteredSoftware.Add(software); 
                 }
             }
 
             return filteredSoftware;
+        }
+
+        private bool IsHigherVersion(int[] version, int[] comparedVersion)
+        {
+            if (comparedVersion.ElementAtOrDefault(0) < version.ElementAtOrDefault(0))
+            {
+                return true; 
+            }
+            else if (comparedVersion.ElementAtOrDefault(0) == version.ElementAtOrDefault(0))
+            {
+                if (comparedVersion.ElementAtOrDefault(1) < version.ElementAtOrDefault(1))
+                {
+                    return true; 
+                }
+                else if (comparedVersion.ElementAtOrDefault(1) == version.ElementAtOrDefault(1))
+                {
+                    if (comparedVersion.ElementAtOrDefault(2) < version.ElementAtOrDefault(2))
+                    {
+                        return true; 
+                    }
+                }
+            }
+
+            return false; 
         }
     }
 }
